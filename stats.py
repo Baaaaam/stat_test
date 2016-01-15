a7230dcf348bbb315167c92f4eb0c0c163d2a6d9
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-01-15 14:16:30 -0600

    add str list mean test and correct stat for it

--- stats.py
+++ stats.py
@@ -2,8 +2,14 @@ def mean(vals):
   "calculate the arithmetic mean of the list"
 
   assert type(vals) is list, 'wrong input format'
-  total = sum(vals)
+
   length = len(vals)
+  parsedlist = list();
+  for i in range(0, length):
+    parsedlist.append(float(vals[i]))
+
+    total = sum(parsedlist)
+
   if length == 0:
     return 0.0
   return total/length
--- test_stats.py
+++ test_stats.py
@@ -3,15 +3,14 @@ from nose.tools import assert_equal
 
 def test_mean():
   assert_equal(mean([2,4]), 3.0)
-#test_mean()
+
 
 def test_empty_list():
   assert_equal( mean([]),0.0)
-#test_empty_list()
+
 
 def test_flaot_mean():
   assert_equal( mean([1.5,1.5,0]), 1)
-#test_flaot_mean()
 
-print(mean([]))
-#print(mean('hello'))
+def test_str_list_mean():
+  assert_equal(mean(['1','2','3']),2.0)
