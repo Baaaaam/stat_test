3ce2bf50d0c89b617d53aa7fa410e04b0d5f4e86
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-01-15 13:34:45 -0600

    add respond and test for empty list

--- stats.py
+++ stats.py
@@ -4,11 +4,18 @@ def mean(vals):
   assert type(vals) is list, 'wrong input format'
   total = sum(vals)
   length = len(vals)
+  if length == 0:
+    return 0.0
   return total/length
 
 def test_mean():
   assert mean([2,4]) == 3.0
 test_mean()
 
-#print(mean([2,4]))
-print(mean('hello'))
+def test_empty_list():
+  assert mean([]) == 0.0
+test_empty_list()
+
+
+print(mean([]))
+#print(mean('hello'))
