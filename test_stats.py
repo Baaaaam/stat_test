c63855d68e67f44f7babee6d02694949fd1c65c1
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-01-15 13:55:35 -0600

    don't know

--- test_stats.py
+++ test_stats.py
@@ -1,15 +1,15 @@
 from stats import mean
 def test_mean():
   assert mean([2,4]) == 3.0
-test_mean()
+#test_mean()
 
 def test_empty_list():
   assert mean([]) == 0.0
-test_empty_list()
+#test_empty_list()
 
 def test_flaot_mean():
   assert(mean([1.5,1.5,0])) == 1
-test_flaot_mean()
+#test_flaot_mean()
 
 print(mean([]))
 #print(mean('hello'))
