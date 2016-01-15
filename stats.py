3aa6882f50b338d1550beadb696665cc761911df
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-01-15 13:22:09 -0600

    check format test

--- stats.py
+++ stats.py
@@ -1,7 +1,10 @@
 def mean(vals):
   "calculate the arithmetic mean of the list"
+
+  assert type(vals) is list, 'wrong input format'
   total = sum(vals)
   length = len(vals)
   return total/length
 
 print(mean([2,4]))
+print(mean('hello'))
