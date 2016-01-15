298cc059b710952dedb97cdcce643182c945fd98
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-01-15 14:01:09 -0600

    asser_equal

--- test_stats.py
+++ test_stats.py
@@ -1,14 +1,16 @@
 from stats import mean
+from nose.tools import assert_equal
+
 def test_mean():
-  assert mean([2,4]) == 3.0
+  assert_equal(mean([2,4]), 3.0)
 #test_mean()
 
 def test_empty_list():
-  assert mean([]) == 0.0
+  assert_equal( mean([]),0.0)
 #test_empty_list()
 
 def test_flaot_mean():
-  assert(mean([1.5,1.5,0])) == 1
+  assert_equal( mean([1.5,1.5,0]), 1)
 #test_flaot_mean()
 
 print(mean([]))
