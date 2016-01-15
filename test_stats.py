68053bb2bd45672b544f1d9cd29df3a7d71f21c9
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-01-15 13:46:50 -0600

    add test function for flaot

--- test_stats.py
+++ test_stats.py
@@ -1,4 +1,4 @@
-from stats import mean  
+from stats import mean
 def test_mean():
   assert mean([2,4]) == 3.0
 test_mean()
@@ -7,6 +7,9 @@ def test_empty_list():
   assert mean([]) == 0.0
 test_empty_list()
 
+def test_flaot_mean():
+  assert(mean([1.5,1.5,0])) == 1
+test_flaot_mean()
 
 print(mean([]))
 #print(mean('hello'))
